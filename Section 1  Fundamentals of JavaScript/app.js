8ac8245714b780e615fa6f9ecd58503c79847b96
Turzhanskyi <turzhansky81@gmail.com>
2020-01-25 18:14:20 +0200

    24. Object descriptor

--- Section 1  Fundamentals of JavaScript/app.js
+++ Section 1  Fundamentals of JavaScript/app.js
@@ -1097,15 +1097,15 @@
 //                              Тема 23. Деструктуризация
 
 
-const user = {
-  firstName: 'Denis',
-  lastName: 'Mescheryakov',
-  age: 25,
-  info: {
-    work: 'EasyCode',
-    skills: ['html', 'css'],
-  },
-};
+// const user = {
+//   firstName: 'Denis',
+//   lastName: 'Mescheryakov',
+//   age: 25,
+//   info: {
+//     work: 'EasyCode',
+//     skills: ['html', 'css'],
+//   },
+// };
 
 // const firstName = user.firstName;
 // const lastName = user.lastName;
@@ -1116,51 +1116,90 @@ const user = {
 // } = user;
 // console.log(skills);
 
-const colors = ['white', 'black', 'red', '#89432d'];
-const [w, b, red, green = 'green'] = colors;
+// const colors = ['white', 'black', 'red', '#89432d'];
+// const [w, b, red, green = 'green'] = colors;
 // console.log(w, b, red, green);
-const names = ['Denis', 'Ivan', 'Maksim'];
-const [name1, ...otherNames] = names;
-console.log(name1, otherNames);
-const nestedArr = ['hello world', ['key', 'value']];
-const [, [key, value]] = nestedArr;
+// const names = ['Denis', 'Ivan', 'Maksim'];
+// const [name1, ...otherNames] = names;
+// console.log(name1, otherNames);
+// const nestedArr = ['hello world', ['key', 'value']];
+// const [, [key, value]] = nestedArr;
 // console.log(key, value);
 // const [...newNames] = names;
-const newNames = [...names];
+// const newNames = [...names];
 // console.log(...newNames);
-const colorNames = ['some value', ...colors, ...names];
+// const colorNames = ['some value', ...colors, ...names];
 // console.log(colorNames);
-const newUser = { ...user, age: 30 };
-console.log(newUser);
-const {
-  info: {
-    skills: [html, css],
-  },
-} = newUser;
+// const newUser = { ...user, age: 30 };
+// console.log(newUser);
+// const {
+//   info: {
+//     skills: [html, css],
+//   },
+// } = newUser;
 
 // console.log(html, css);
 
-function myPerson({
-  lastName = '&',
-  firstName = '&',
-  info: { skills } = {},
-} = {}) {
-  console.log(skills);
-}
+// function myPerson({
+//   lastName = '&',
+//   firstName = '&',
+//   info: { skills } = {},
+// } = {}) {
+//   console.log(skills);
+// }
 
 // myPerson(newUser);
-function foo(x, y, ...others) {
-  console.log(arguments);
-  // console.log(Array.prototype.slice.call(arguments));
-  const [...args] = arguments;
-  console.log(others);
-}
+// function foo(x, y, ...others) {
+//   console.log(arguments);
+//   // console.log(Array.prototype.slice.call(arguments));
+//   const [...args] = arguments;
+//   console.log(others);
+// }
 
 // foo(1, 2, 5, 6);
-const numbers = [2, 3];
+// const numbers = [2, 3];
 
-function foo2(x, y) {
-  console.log(x, y);
+// function foo2(x, y) {
+//   console.log(x, y);
+// }
+
+// foo2(...numbers);
+
+
+
+//                              Тема 24. Object descriptor
+
+
+
+const car = {
+  brand: 'Audi',
+  year: 2019,
+  get age() {
+    return `Мышина выпущена в ${this.year}`;
+  },
+  set age(value) {
+    this.year = value;
+  },
+};
+
+// Object.defineProperty(car, 'age', {
+//   // configurable: false,
+//   // enumerable: false,
+//   // writable: false,
+//   get: function() {
+//     return `Мышина выпущена в ${this.year}`;
+//   },
+// });
+
+Object.defineProperty(window, 'globalVar', {
+  set: function (value) {
+    console.log(value);
+    debugger;
+  },
+});
+
+function foo() {
+  globalVar = 10;
 }
 
-foo2(...numbers);
+foo();
