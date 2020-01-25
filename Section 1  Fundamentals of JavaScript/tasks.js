5e17aae6fd938b0ae9812389e63d00d6a6e8a22c
Turzhanskyi <turzhansky81@gmail.com>
2020-01-25 12:02:03 +0200

    10. Шаблонные строки

--- Section 1  Fundamentals of JavaScript/tasks.js
+++ Section 1  Fundamentals of JavaScript/tasks.js
@@ -34,3 +34,59 @@
 
 // let strToNumber = parseFloat("100$");
 // console.log(strToNumber);
+
+
+
+//                              Тема 10. Шаблонные строки
+
+
+
+// let string = ‘some test string’;
+// ВРУЧНУЮ НИЧЕГО НЕ СЧИТАТЬ
+
+// 10.1. Получить первую и последнюю буквы строки
+
+let string = 'some test string';
+
+let task1 = `перша буква ${string[0]}, а остання - ${string[string.length - 1]}`;
+console.log(task1);
+
+// 10.2. Сделать первую и последнюю буквы в верхнем регистре
+
+let task2 = string[0].toUpperCase() + string.slice(1, -1) + string[string.length - 1].toUpperCase();
+console.log(task2);
+
+// 10.3. Найти положение слова ‘string’ в строке
+
+let task3 = string.indexOf('string');
+console.log(task3);
+
+// 10.4. Найти положение второго пробела (“вручную” ничего не считать)
+
+let task4 = string.lastIndexOf(' ');
+console.log(task4);
+
+// 10.5. Получить строку с 5-го символа длиной 4 буквы
+
+let task5 = string.substr(5, 4);
+console.log(task5);
+
+// 10.6. Получить строку с 5-го по 9-й символы
+
+let task6 = string.substring(5, 9);
+console.log(task6);
+
+// 10.7. Получить новую строку из исходной путем удаления последних 6-и символов
+// (то есть исходная строка без последних 6и символов)
+
+let task7 = string.substring(0, (string.length - 6));
+console.log(task7);
+
+
+// 8. Из двух переменных a=20 и b=16 получить переменную string, в которой будет
+// содержаться текст “2016”
+
+let a = 20;
+let b = 16;
+string = a.toString() + b.toString();
+console.log(string);
