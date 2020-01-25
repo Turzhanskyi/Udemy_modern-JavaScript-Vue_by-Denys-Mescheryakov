22009bb710f5962f3198b7c98443330a18c1b92e
Turzhanskyi <turzhansky81@gmail.com>
2020-01-25 13:35:15 +0200

    14. Циклы

--- Section 1  Fundamentals of JavaScript/tasks.js
+++ Section 1  Fundamentals of JavaScript/tasks.js
@@ -268,29 +268,84 @@
 
 // Записать условие, используя конструктор switch. В консоли должно отразиться только одно значение.
 
-let a = 'block';
-switch (a) {
-  case 'block':
-    console.log(a);
-    break;
-  case 'none':
-    console.log(a);
-    break;
-  case 'inline':
-    console.log(a);
-    break;
-  default:
-    console.log('other');
-}
+// let a = 'block';
+// switch (a) {
+//   case 'block':
+//     console.log(a);
+//     break;
+//   case 'none':
+//     console.log(a);
+//     break;
+//   case 'inline':
+//     console.log(a);
+//     break;
+//   default:
+//     console.log('other');
+// }
 
 // 13.2. Из задач по условному оператору if else выполнить задачи 1 и 2 в виде тернарного оператора.
 
-let b = 'inline1';
+// let b = 'inline1';
 
-b === 'block' ? console.log('block') : b === 'none' ? console.log('none') : b === 'inline' ? console.log('inline') : console.log('other');
+// b === 'block' ? console.log('block') : b === 'none' ? console.log('none') : b === 'inline' ? console.log('inline') : console.log('other');
 
-var firstCheck = false,
-  secondCheck = false,
-  access = firstCheck ? "Доступ запрещен" : secondCheck ? "Доступ запрещен" : "Доступ разрешен";
+// var firstCheck = false,
+//   secondCheck = false,
+//   access = firstCheck ? "Доступ запрещен" : secondCheck ? "Доступ запрещен" : "Доступ разрешен";
 
-console.log(access);
+// console.log(access);
+
+
+
+//                              Тема 14. Циклы
+
+
+
+// 14.1. На основе строки “i am in the easycode” сделать новую строку где первые буквы каждого слова 
+// будут в верхнем регистре. Использовать for или while.
+
+const str = 'i am in the easycode';
+let newStr = '';
+
+for (let i = 0; i < str.length; i++) {
+  str[i - 1] === ' ' || i === 0 ? newStr += str[i].toUpperCase() : newStr += str[i];
+}
+
+console.log(newStr);
+
+// 14.2. Дана строка “tseb eht ma i”. Используя циклы, сделать строку-перевертыш (то есть последняя буква становится первой, предпоследняя - второй итд).
+
+
+
+// 14.3. Факториал числа - произведение всех натуральных чисел от 1 до n
+// включительно: 3! = 3*2*1, 5! = 5*4*3*2*1. С помощью циклов вычислить факториал числа 10. Использовать for.
+
+
+
+// 14.4. На основе строки “JavaScript is a pretty good language” сделать новую строку,
+// где каждое слово начинается с большой буквы, а пробелы удалены. Использовать for.
+
+const str1 = 'JavaScript is a pretty good language';
+let newStr1 = '';
+
+for (let i = 0; i < str1.length; i++) {
+  if (str1[i] === ' ') {
+    continue
+  } else {
+    newStr1 += str[i];
+  }
+}
+
+console.log(newStr1);
+
+// 14.5. Найти все нечетные числа в массиве от 1 до 15 включительно и вывести их в консоль. Массив [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15] Использовать for of.
+
+
+
+// 14.6. Дан объект:
+// let list = {
+//      name: ‘denis’,
+//      work: ‘easycode’,
+//      age: 29
+// }
+// Перебрать объект и если значение в свойстве это строка то переписать ее всю в верхнем регистре. Использовать for in.
