a47459494aa7a2335c18d94f3e041aa467be3a9b
Turzhanskyi <turzhansky81@gmail.com>
2020-01-25 17:45:21 +0200

    20. Перебирающие методы массивов

--- Section 1  Fundamentals of JavaScript/tasks.js
+++ Section 1  Fundamentals of JavaScript/tasks.js
@@ -714,15 +714,190 @@
 
 // 19.1. Переделать функцию с использованием функции-стрелки (в методе reduce тоже использовать arrow function):
 
-function sum() {
-  const params = Array.prototype.slice.call(arguments);
+// function sum() {
+//   const params = Array.prototype.slice.call(arguments);
 
-  if (!params.length) return 0;
+//   if (!params.length) return 0;
 
-  return params.reduce(function (prev, next) {
-    return prev + next;
-  });
+//   return params.reduce(function (prev, next) {
+//     return prev + next;
+//   });
+// }
+
+// console.log(sum(1, 2, 3, 4));
+// console.log(sum());
+
+
+
+//                              Тема 20. Перебирающие методы массивов
+
+
+
+// 20.1. На основе массива [1,2,3,5,8,9,10] сформировать новый массив,
+// каждый элемент которого будет хранить информацию о числе и его четности:
+// [{digit: 1, odd: true}, {digit: 2, odd: false}, {digit: 3, odd: true}...]
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8];
+
+const newArr = arr.map(num => ({
+  digit: num,
+  odd: num % 2 !== 0,
+}));
+
+console.log(newArr);
+
+
+// 20.2. Проверить, содержит ли массив [12, 4, 50, 1, 0, 18, 40] элементы, равные нулю. Если да - вернуть true.
+
+const arr1 = [12, 4, 50, 1, 0, 18, 40];
+
+function isZero(num) {
+  return num === 0;
 }
 
-console.log(sum(1, 2, 3, 4));
-console.log(sum());
+console.log(arr1.some(isZero));
+
+
+// 20.3. Проверить, содержит ли массив ['yes', 'hello', 'no', 'easycode', 'what'] хотя бы одно слово длиной больше 3х букв. Если да - вернуть true
+
+const arr2 = ['yes', 'hello', 'no', 'easycode', 'what'];
+
+
+console.log(arr2.some(arr2.lenght > 3));
+
+
+// 20.4. Дан массив объектов, где каждый объект содержит информацию о букве и месте её положения в строке {буква: “a”, позиция_в_предложении: 1}:
+// [{char:"a",index:12}, {char:"w",index:8}, {char:"Y",index:10}, {char:"p",index:3}, {char:"p",index:2},
+// {char:"N",index:6}, {char:" ",index:5}, {char:"y",index:4}, {char:"r",index:13}, {char:"H",index:0},
+// {char:"e",index:11}, {char:"a",index:1}, {char:" ",index:9}, {char:"!",index:14}, {char:"e",index:7}]
+// Напишите функцию, которая из элементов массива соберет и вернёт
+// строку, основываясь на index каждой буквы. Например:
+// [{char:"H",index:0}, {char:"i",index: 1}, {char:"!",index:2}] → “Hi!”
+
+const stringArr = [{
+    char: "a",
+    index: 12
+  }, {
+    char: "w",
+    index: 8
+  }, {
+    char: "Y",
+    index: 10
+  }, {
+    char: "p",
+    index: 3
+  }, {
+    char: "p",
+    index: 2
+  },
+  {
+    char: "N",
+    index: 6
+  }, {
+    char: " ",
+    index: 5
+  }, {
+    char: "y",
+    index: 4
+  }, {
+    char: "r",
+    index: 13
+  }, {
+    char: "H",
+    index: 0
+  },
+  {
+    char: "e",
+    index: 11
+  }, {
+    char: "a",
+    index: 1
+  }, {
+    char: " ",
+    index: 9
+  }, {
+    char: "!",
+    index: 14
+  }, {
+    char: "e",
+    index: 7
+  }
+];
+
+function getStringRFromArray(arr) {
+  if (!Array.isArray(arr)) {
+    return console.log('it is not an array')
+  }
+  return arr
+    .slice()
+    .sort((prev, next) => prev.index - next.index)
+    .reduce((acc, current) => (acc += current.char), '');
+}
+
+console.log(getStringRFromArray(stringArr));
+
+// 20.5. Отсортируйте массив массивов так, чтобы вначале располагались наименьшие массивы (размер массива определяется его длиной): [  [14, 45],  [1],  ['a', 'c', 'd']  ] → [ [1], [14, 45], ['a', 'c', 'd'] ]
+
+
+
+
+// 20.6. Есть массив объектов:
+// [
+//     {cpu: 'intel', info: {cores:2, сache: 3}},
+//     {cpu: 'intel', info: {cores:4, сache: 4}},
+//     {cpu: 'amd', info: {cores:1, сache: 1}},
+//     {cpu: 'intel', info: {cores:3, сache: 2}},
+//     {cpu: 'amd', info: {cores:4, сache: 2}}
+// ]
+// Отсортировать их по возрастающему количеству ядер (cores).
+
+
+
+// 20.7. Создать функцию, которая будет принимать массив продуктов и две цены. Функция должна вернуть все продукты, цена которых находится в указанном диапазоне, и сортировать от дешевых к дорогим:
+// let products = [
+//     {title: 'prod1', price: 5.2}, {title: 'prod2', price: 0.18},
+//     {title: 'prod3', price: 15}, {title: 'prod4', price: 25},
+//     {title: 'prod5', price: 18.9}, {title: 'prod6', price: 8},
+//     {title: 'prod7', price: 19}, {title: 'prod8', price: 63}
+// ];
+
+// filterCollection(products, 15, 30) → [{...price: 15}, {...price: 18.9}, {...price: 19}, {...price: 25}]
+
+let products = [{
+    title: 'prod1',
+    price: 5.2
+  }, {
+    title: 'prod2',
+    price: 0.18
+  },
+  {
+    title: 'prod3',
+    price: 15
+  }, {
+    title: 'prod4',
+    price: 25
+  },
+  {
+    title: 'prod5',
+    price: 18.9
+  }, {
+    title: 'prod6',
+    price: 8
+  },
+  {
+    title: 'prod7',
+    price: 19
+  }, {
+    title: 'prod8',
+    price: 63
+  }
+];
+
+function filterCollection(arr, min, max) {
+  return arr
+    .slice()
+    .sort((prev, next) => prev.price - next.price)
+    .filter(product => product.price >= min && product.price <= max);
+}
+
+console.log(filterCollection(products, 15, 30));
