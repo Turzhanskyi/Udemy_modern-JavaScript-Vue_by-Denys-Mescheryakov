c5ad93d2402077d0edde08eca35bfcef38007477
Turzhanskyi <turzhansky81@gmail.com>
2020-01-25 17:55:30 +0200

    22. Методы объектов про которые часто забывают

--- Section 1  Fundamentals of JavaScript/app.js
+++ Section 1  Fundamentals of JavaScript/app.js
@@ -1040,19 +1040,54 @@
 // res[0]();
 // res[5]();
 
-var x = 10;
+// var x = 10;
 
-function foo(a) {
-  var b = 20;
+// function foo(a) {
+//   var b = 20;
 
-  function bar(c) {
-    var d = 30;
-    return boop(x + a + b + c + d);
-  }
+//   function bar(c) {
+//     var d = 30;
+//     return boop(x + a + b + c + d);
+//   }
 
-  function boop(e) {
-    return e * -1;
-  }
+//   function boop(e) {
+//     return e * -1;
+//   }
 
-  return bar;
-}
+//   return bar;
+// }
+
+
+
+//                              Тема 22. Методы объектов про которые часто забывают.
+
+
+
+let obj1 = {
+  name: 'Denis',
+  info: {
+    skills: ['html', 'css'],
+  },
+};
+
+let obj2 = {
+  name: 'Ivan',
+  age: 20,
+};
+
+let newObj = Object.assign({}, obj2, obj1);
+newObj = Object.assign({}, obj1);
+newObj = JSON.parse(JSON.stringify(obj1));
+// console.log(newObj.info === obj1.info);
+
+let keys = Object.keys(obj2);
+console.log(keys);
+let values = Object.values(obj2);
+console.log(values);
+let entries = Object.entries(obj2);
+console.log(entries);
+let fromEntries = Object.fromEntries([
+  ['a', 'value'],
+  ['b', 'b_value']
+]);
+console.log(fromEntries);
