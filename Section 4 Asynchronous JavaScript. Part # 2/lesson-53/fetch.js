eae09f1c4eaa51feb49e954e7a26113794b5f75f
Turzhanskyi <turzhansky81@gmail.com>
2020-01-31 10:04:35 +0200

    53. Введение в fetch

---
+++ Section 4 Asynchronous JavaScript. Part # 2/lesson-53/fetch.js
@@ -0,0 +1,40 @@
+// fetch('https://jsonplaceholder.typicode.com/posts')
+//   .then(response => {
+//     return response.json();
+//   })
+//   .then(posts => console.log(posts))
+//   .catch(err => console.log(err));
+
+
+function getPost(id) {
+  return new Promise((resolve, reject) => {
+    fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+      .then(response => response.json())
+      .then(post => resolve(post))
+      .catch(err => reject(err));
+  });
+}
+
+// getPost(2).then(post => console.log(post));
+
+function getpost2(id) {
+  const [userType, userId] = id.split('-');
+  return fetch(`https://jsonplaceholder.typicode.com/posts/${userId}`)
+    .then(response => response.json(), );
+}
+
+// getpost2('user-23')
+//   .then(post => console.log(post))
+//   .catch(err => console.log(err));
+
+function getPost3(id) {
+  return Promise.resolve().then(() => {
+    const [userType, userId] = id.split('-');
+    return fetch(`https://jsonplaceholder.typicode.com/posts/${userId}`)
+      .then(response => response.json(), );
+  });
+}
+
+getPost3('user-23')
+  .then(post => console.log(post))
+  .catch(err => console.log(err));
