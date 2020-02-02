3b5dc14f938241179f23a849a15e403ecd2e66ec
Turzhanskyi <turzhansky81@gmail.com>
2020-02-02 21:18:54 +0200

    58. Prototype ES5

--- Section 5 OOP/oop.js
+++ Section 5 OOP/oop.js
@@ -5,17 +5,30 @@ function Product(brand, price, discount) {
   this.brand = brand;
   this.price = price;
   this.discount = discount;
-  this.getPriceWithDiscount  = function() {
-    return (this.price * (100 - this.discount)) / 100;
-  };
+}
+
+Product.prototype.getPriceWithDiscount = function() {
+  return (this.price * (100 - this.discount)) / 100;
+};
+
+Product.prototype.setPrice = function(newPrice) {
+  this.price = newPrice;
 }
 
 const apple = new Product('Apple', 100, 15);
 const samsung = new Product('Samsung', 200, 25);
 
-console.log(apple);
-console.log(samsung);
+// console.log(apple);
+// console.log(samsung);
 
 console.log(apple.getPriceWithDiscount());
 
 console.log(apple instanceof Product);
+
+const apple1 = apple.setPrice(500);
+
+console.log(apple);
+
+console.log(apple1);
+
+console.log(apple.getPriceWithDiscount());
